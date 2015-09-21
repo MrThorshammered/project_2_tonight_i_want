543f07779fac31a3fbbfc50db80dc5391b187948
Mike Jones <mikethespike77@gmail.com>
2015-09-21 16:41:40 +0100

    delete now working

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,13 @@
 class RecipesController < ApplicationController
 
 
+	def index
+	end
+
+
 	def show
-		@countries = Country.find(params[:id])
+		# binding.pry
+		@country = Country.find(params[:id])
 	end
 
 
@@ -11,19 +16,30 @@ class RecipesController < ApplicationController
 	end
 
 	def create
+		# binding.pry
 		@recipe = Recipe.new(recipe_params)
       if @recipe.save
         redirect_to recipe_path(recipe_params[:country_id])
       else
          redirect_to :new
       end
-
 	end
 
+	def edit
+	end
 
+	def update
+	end
 
+	def destroy
+		# country = Country.find(params[:country_id])
+		@recipe = Recipe.find(params[:id]).destroy
+		redirect_to :back
+	end
 
   private
+
+
     def recipe_params
       params.require(:recipe).permit(:name, :method, :recipe_image, :country_id)
     end
